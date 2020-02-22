ef850e932cd6ccbb11fec6c4a5e603eec39f6945
NativeCode <opensource@nativecode.com>
2020-02-21 19:39:36 -0500

    chore: fix build

--- binaries/dockerhub/src/TagResolver.ts
+++ binaries/dockerhub/src/TagResolver.ts
@@ -2,8 +2,8 @@ import { Tag } from './Tag'
 
 export function tagResolver(tags: Tag[]): Tag[] {
   return tags.map(tag => {
-    if (/[a-zA-Z]+/g.test(tag.repository.name)) {
-      const matched = tags.find(x => tag.repository.full_size === x.repository.full_size)
+    if (/[a-zA-Z]+/g.test(tag.repotag.name)) {
+      const matched = tags.find(x => tag.repotag.full_size === x.repotag.full_size)
 
       if (matched) {
         tag.version = matched.version
