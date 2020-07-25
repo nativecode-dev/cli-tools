b15c31509aa4acb20418a68e2511cafc424ec114
NativeCode <opensource@nativecode.com>
2020-07-25 01:56:46 -0400

    feat: dependency upgrades

--- binaries/dockerhub/src/Resources/Tags.ts
+++ binaries/dockerhub/src/Resources/Tags.ts
@@ -1,11 +1,20 @@
+import { ResourceParamType } from '@nativecode/rest-client'
+
 import { RepositoryTag } from '../Models/RepositoryTag'
 import { DockerHubResults } from '../Models/DockerHubResults'
 import { DockerHubResource } from '../DockerHubResource'
-import { ResourceParamType } from '@nativecode/rest-client'
 
 export class Tags extends DockerHubResource {
-  list(repository: string, size: number = 1000): Promise<DockerHubResults<RepositoryTag>> {
+  async list(repository: string, size: number = 100): Promise<DockerHubResults<RepositoryTag>> {
     const repo = repository.indexOf('/') > 0 ? repository : `library/${repository}`
+
+    const blah = await this.response(`repositories/${repo}/tags/`, 'GET', [
+      {
+        key: 'page_size',
+        type: ResourceParamType.Query,
+        value: size,
+      },
+    ])
 
     return this.http_get<DockerHubResults<RepositoryTag>>(`repositories/${repo}/tags/`, {
       key: 'page_size',
